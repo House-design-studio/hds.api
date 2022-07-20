da9f29c201ac6f9178b4501daee11d139c0dd1b3
Mikhail Godunko <mikhail.godunko@outlook.com>
2022-07-20 06:11:49 +0300

    класс Shrinkage, тесты, xml документация
    был добавлен класс Shrinkage для расчёта усушки пиломатериала по госту. Прошёл соответствующие unit тесты.
    
    была добавлена xml документация для классов и их методов

--- website.BusinessLogic/Math/Math.cs
+++ website.BusinessLogic/Math/Math.cs
@@ -1,8 +1,23 @@
 ﻿namespace website.BusinessLogic
 {
+    /// <summary>
+    /// Класс для математический рассчётов
+    /// </summary>
     public static partial class Math
     {
+        /// <summary>
+        /// Точка в трёхмерном пространстве
+        /// </summary>
+        /// <param name="X">координата X</param>
+        /// <param name="Y">координата Y</param>
+        /// <param name="Z">координата Z</param>
         public record struct Point3D(double X, double Y, double Z);
+
+        /// <summary>
+        /// Точка в двухмерном пространстве
+        /// </summary>
+        /// <param name="X">координата X</param>
+        /// <param name="Y">координата Y</param>
         public record struct Point2D(double X, double Y);
     }
 }
---
+++ website.Tests/Beam/ShrinkageTests.cs
@@ -0,0 +1,39 @@
+﻿using static website.BusinessLogic.Beam.Shrinkage;
+
+namespace website.Tests.Beam
+{
+    internal class ShrinkageTests
+    {
+        [Test]
+        public void Get_Shrinkage_Returns_Table()
+        {
+            var result = GetShrinkage(40 * 0.001);
+
+            Assert.That(result, Is.EqualTo(1.7 * 0.001).Within(0.001));
+        }
+
+        [Test]
+        public void Get_Shrinkage_Returns_Between()
+        {
+            var result = GetShrinkage(145 * 0.001);
+
+            Assert.That(result, Is.EqualTo(5.85 * 0.001).Within(0.001));
+        }
+
+        [Test]
+        public void Get_Shrinkage_Returns_Before()
+        {
+            var result = GetShrinkage(10 * 0.001);
+
+            Assert.That(result, Is.EqualTo(0.5 * 0.001).Within(0.001));
+        }
+
+        [Test]
+        public void Get_Shrinkage_Returns_After()
+        {
+            var result = GetShrinkage(310 * 0.001);
+
+            Assert.That(result, Is.EqualTo(11.1 * 0.001).Within(0.001));
+        }
+    }
+}
