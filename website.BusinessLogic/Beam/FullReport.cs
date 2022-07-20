4a9003b6249c015ae976be33258dcb7eb4d5ec6e
godunko-mikhail <mikhail.godunko@vk.com>
2022-07-20 10:30:59 +0300

    отображение величин усушки

--- website.BusinessLogic/Beam/FullReport.cs
+++ website.BusinessLogic/Beam/FullReport.cs
@@ -9,6 +9,8 @@ namespace website.BusinessLogic.Beam
         public double ShrinkageInWidth { get; set; }
         public double ShrinkageInHeight { get; set; }
 
+        public double EstimatedWidth { get; set; }
+        public double EstimatedHeight { get; set; }
 
         public FullReport(Input input)
         {
@@ -16,6 +18,9 @@ namespace website.BusinessLogic.Beam
             
             this.ShrinkageInWidth = Shrinkage.GetShrinkage(input.Width);
             this.ShrinkageInHeight = Shrinkage.GetShrinkage(input.Height);
+
+            this.EstimatedWidth = Input.Width - this.ShrinkageInWidth;
+            this.EstimatedHeight = Input.Height - this.ShrinkageInHeight;
         }
     }
 }
