58b49cad4056df22436705fbd286ca705dbb1ce4
godunko-mikhail <mikhail.godunko@vk.com>
2022-07-20 11:33:26 +0300

    Полярный момент инерции сечения

--- website.BusinessLogic/Beam/Analyze/Analyze.cs
+++ website.BusinessLogic/Beam/Analyze/Analyze.cs
@@ -2,10 +2,26 @@ using static website.BusinessLogic.Math;
 namespace website.BusinessLogic.Beam
 {
     /// <summary>
-    /// Класс для расчёт величины усушки 
+    /// Класс для расчётов планки 
     /// </summary>
     public static partial class Analyze
     {
+        /// <summary>
+        /// Расчёт площади поперечного сечения
+        /// </summary>
+        /// <param name="width">ширина</param>
+        /// <param name="height">высота</param>
+        /// <returns>площадь поперечного сечения</returns>
+        public static double GetCrossSectionArea(double width, double height) => 
+            width * height;
 
+        /// <summary>
+        /// Расчёт полярного момента инерции сечения
+        /// </summary>
+        /// <param name="width">ширина</param>
+        /// <param name="height">высота</param>
+        /// <returns>Полярный момент инерции сечения</returns>
+        public static double GetPolarMomentOfInertia(double width, double height) => 
+            width * height * (width * width + height * height) / 12;
     }
 }
