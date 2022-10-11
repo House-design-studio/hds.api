ad93cfc184fe5338b32645db69a0004ff1cec7a6
mikhail-godunko <mikhail.godunko@vk.com>
2022-10-11 09:35:51 +0300

    add: добавление точек fem api разделено, распред. нагрузки

--- website.BusinessLogic/Beam/BeamCalculator.cs
+++ website.BusinessLogic/Beam/BeamCalculator.cs
@@ -64,79 +64,130 @@ namespace HDS.BusinessLogic.Beam
         private async Task SetApiData()
         {
             var nodes = new List<FemClientRequest.Node>();
-            FemClientRequest.Node tmp;
-            // начальная точка
-            nodes.Add(new FemClientRequest.Node());
-            // конечная точка
-            nodes.Add(new FemClientRequest.Node(
-                new Mathematics.Point3D(_report.Input.Length, 0, 0)));
 
-            // опоры
-            var supports = _report.Input.Supports.ToArray();
-            Array.Sort(supports);
-            for (int i = 0; i < supports.Length; i++)
-            {
-                tmp = new FemClientRequest.Node(
-                    new Mathematics.Point3D(supports[i], 0, 0));
-                tmp.Support.X = (i == 0);
-                tmp.Support.Y = true;
-                tmp.Support.Z = true;
-                nodes.Add(tmp);
-            }
+            AddBaseDots();
+            AddAdditionsDots();
+            nodes = nodes.DistinctBy(n => n.Coordinate.X).ToList();
 
-            // распределённые нагрузки
-            foreach (var load in _report.Input.DistributedLoads)
-            {
-                nodes.Add(new FemClientRequest.Node(new Mathematics.Point3D(load.OffsetStart, 0, 0)));
-                nodes.Add(new FemClientRequest.Node(new Mathematics.Point3D(load.OffsetEnd, 0, 0)));
-            }
-
-            // сосредоточенные нагрузки
-            foreach (var load in _report.Input.ConcentratedLoads)
-            {
-                nodes.Add(new FemClientRequest.Node(new Mathematics.Point3D(load.Offset, 0, 0)));
-            }
-
+            SetValues();
             nodes = nodes.OrderBy(n => n.Coordinate.X).ToList();
-            var nodesArray = nodes.ToArray();
-            // вставка доп точек
-            // не менее 3х между важными с шагом не более 0.05 метра (5см)
-            for (int i = 0; i < nodesArray.Length - 1; i++)
+
+            Console.WriteLine("asdasda");
+
+
+            void AddBaseDots()
             {
-                var distance = nodesArray[i + 1].Coordinate.X - nodesArray[i].Coordinate.X;
-                if (distance <= 0.01) continue;
+                // начальная точка
+                nodes.Add(new FemClientRequest.Node());
 
-                var newNodes = new List<FemClientRequest.Node>();
+                // конечная точка
+                nodes.Add(new FemClientRequest.Node(
+                    new Mathematics.Point3D(_report.Input.Length, 0, 0)));
 
-                if (distance > 0.01 && distance <= 0.2)
+                // опоры
+                foreach (var supportOffset in _report.Input.Supports)
                 {
-                    newNodes.Add(new FemClientRequest.Node(
-                        new Mathematics.Point3D(nodesArray[i].Coordinate.X + (distance / 4), 0, 0)));
-
-                    newNodes.Add(new FemClientRequest.Node(
-                        new Mathematics.Point3D(nodesArray[i].Coordinate.X + (distance / 4 * 2), 0, 0)));
-
-                    newNodes.Add(new FemClientRequest.Node(
-                        new Mathematics.Point3D(nodesArray[i].Coordinate.X + (distance / 4 * 3), 0, 0)));
+                    nodes.Add(new FemClientRequest.Node(new Mathematics.Point3D(supportOffset, 0, 0)));
                 }
-                else if (distance > 0.2)
-                {
-                    int numOfPoints = (int)Math.Ceiling(distance / 0.05) - 1;
-                    var distanceBetween = distance / numOfPoints;
 
-                    for (int j = 0; j < numOfPoints; j++)
+                // распределённые нагрузки
+                foreach (var load in _report.Input.DistributedLoads)
+                {
+                    nodes.Add(new FemClientRequest.Node(new Mathematics.Point3D(load.OffsetStart, 0, 0)));
+                    nodes.Add(new FemClientRequest.Node(new Mathematics.Point3D(load.OffsetEnd, 0, 0)));
+                }
+
+                // сосредоточенные нагрузки
+                foreach (var load in _report.Input.ConcentratedLoads)
+                {
+                    nodes.Add(new FemClientRequest.Node(new Mathematics.Point3D(load.Offset, 0, 0)));
+                }
+            }
+            void AddAdditionsDots()
+            {
+                // вставка доп точек
+                // не менее 3х между важными с шагом не более 0.05 метра (5см)
+                var nodesArray = nodes.ToArray();
+                for (int i = 0; i < nodesArray.Length - 1; i++)
+                {
+                    var distance = nodesArray[i + 1].Coordinate.X - nodesArray[i].Coordinate.X;
+                    if (distance <= 0.01) continue;
+
+                    var newNodes = new List<FemClientRequest.Node>();
+
+                    if (distance > 0.01 && distance <= 0.2)
                     {
                         newNodes.Add(new FemClientRequest.Node(
-                        new Mathematics.Point3D(nodesArray[i].Coordinate.X + distanceBetween * j, 0, 0)));
+                            new Mathematics.Point3D(nodesArray[i].Coordinate.X + (distance / 4), 0, 0)));
+
+                        newNodes.Add(new FemClientRequest.Node(
+                            new Mathematics.Point3D(nodesArray[i].Coordinate.X + (distance / 4 * 2), 0, 0)));
+
+                        newNodes.Add(new FemClientRequest.Node(
+                            new Mathematics.Point3D(nodesArray[i].Coordinate.X + (distance / 4 * 3), 0, 0)));
                     }
+                    else if (distance > 0.2)
+                    {
+                        int numOfPoints = (int)Math.Ceiling(distance / 0.05) + 1;
+                        var distanceBetween = distance / (numOfPoints - 1);
+
+                        for (int j = 0; j < numOfPoints; j++)
+                        {
+                            newNodes.Add(new FemClientRequest.Node(
+                            new Mathematics.Point3D(nodesArray[i].Coordinate.X + distanceBetween * j, 0, 0)));
+                        }
+                    }
+                    nodes.InsertRange(i, newNodes);
+                }
+            }
+            void SetValues()
+            {
+                // опоры
+                var supportsArray = _report.Input.Supports.OrderBy(s => s).ToArray();
+                for (int i = 0; i < supportsArray.Length; i++)
+                {
+                    double supportOffset = _report.Input.Supports[i];
+                    var supportViaNode = nodes.First(n => n.Coordinate.X == supportOffset);
+                    supportViaNode.Support.X = (i == 0);
+                    supportViaNode.Support.Y = true;
+                    supportViaNode.Support.Z = true;
                 }
 
-                nodes.InsertRange(i + 1, newNodes);
+                // сосредоточенные нагрузки
+                foreach (var load in _report.Input.ConcentratedLoads)
+                {
+                    var concentratedLoadsViaNode = nodes.First(n => n.Coordinate.X == load.Offset);
+
+                    concentratedLoadsViaNode.Load.Z += -load.LoadForFirstGroup;
+                }
+
+                // распределённые нагрузки
+                foreach (var load in _report.Input.DistributedLoads)
+                {
+                    var nodesBetweenLoad = nodes.Where(node => node.Coordinate.X >= load.OffsetStart &&
+                                                               node.Coordinate.X <= load.OffsetEnd)
+                                                .OrderBy(node => node.Coordinate.X)
+                                                .ToArray();
+
+                    for (int i = 0; i < nodesBetweenLoad.Length - 1; i++)
+                    {
+                        var leftNode = nodesBetweenLoad[i];
+                        var rightNode = nodesBetweenLoad[i + 1];
+                        var l = leftNode.Coordinate.X - rightNode.Coordinate.X;
+                        var F = load.LoadForFirstGroup * l / 2;
+                        var M = load.LoadForFirstGroup * l * l / 12;
+
+                        leftNode.Load.Z += -F;
+                        rightNode.Load.Z += -F;
+
+                        leftNode.Load.V += M;
+                        rightNode.Load.V += -M;
+                    }
+                }
             }
 
             //var request = new FemClientRequest();
             //var response = await _femClient.DoRequest(req);
-
         }
     }
 }
