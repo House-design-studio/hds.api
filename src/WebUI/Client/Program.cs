6b230993bba06aae8b7dcee85f23b4949ecf9360
Godunko Mikhail <mikhail.godunko@vk.com>
2022-12-29 17:11:18 +0300

    add project references and namespaces

--- src/WebUI/Client/Program.cs
+++ src/WebUI/Client/Program.cs
@@ -1,11 +1,18 @@
-using HDS.Client;
+using HDS.WebUI.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace HDS.WebUI.Client;
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+}
